e1c178801fdfb984faba7612d77d64d6dfff0498
Ishant Dahiya <ishantd.ic18@nsut.ac.in>
2020-03-21 01:24:28 +0530

    dynamic dates

---
+++ static/js/new_field.js
@@ -0,0 +1,6 @@
+function create() {
+    console.log("TEST CLIKC")
+    // clonedInput = $('input0').clone();
+    // $(clonedDiv).attr('id', '')
+
+}
--- static/js/weeks.js
+++ static/js/weeks.js
@@ -29,6 +29,7 @@ w = n.getWeek();
 
 var curr = new Date; // get current date
 var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
+// console.log(first)
 var last = first + 6; // last day is the first day + 6
 
 var firstday = new Date(curr.setDate(first)).toUTCString();
@@ -38,12 +39,16 @@ x = document.getElementById("week").innerHTML = firstday + "-" + lastday
 }
 
 days = ["saturday", "sunday", "monday", "tuesday", "wednesday", "thursday", "friday" ]
-console.log(days)
+// console.log(days)
 for (var i = 0; i<days.length; i++) {
-  x = document.getElementById(days[i])
+  
+  var x = document.getElementById(days[i])
   console.log(x)
-  y = firstday + i
-  x.innerHTML = y
-  console.log(firstday+i)
+  // console.log(x)
+  var now = new Date; // get current date
+  var day = now.getDate() - now.getDay(); 
+  var y = day + i
+  x.innerHTML = new Date(now.setDate(y)).toUTCString();
+  // console.log(firstday+i)
 }
 // console.log(x)
