fcbf320daae941ef513c0cda78c597c847c89aa4
Ishant Dahiya <ishantd.ic18@nsut.ac.in>
2020-04-19 19:29:49 +0530

    serialized array data department

--- static/js/dep_assignment.js
+++ static/js/dep_assignment.js
@@ -1,7 +1,9 @@
 $(".project-info").hide();
 $("#hour-assign").hide();
 var hours = ""
+var project;
 $('#project').on('change', function() {
+    project = parseInt(this.value)
     const sel_id = "#" + (this.value).trim()
     $(sel_id).show();
     $("#hour-assign").show();
@@ -21,6 +23,26 @@ $(document).on("change", ".hour-input", function() {
     });
     $("#entered-hours").val(sum);
     $("#left-hours").val(hours - sum)
+});
+
+$('#dep_form').submit(function(e){
+  e.preventDefault();
+  var myform = $("form")
+  var data = myform.serializeArray();
+  var dept_data = [];
+  for (var i=0; i<data.length; i++) {
+    var dataObject = {};
+    var department_name = data[i].name;
+    var time_allocated = parseInt(data[i].value);
+    dataObject = {
+      department_name: department_name,
+      project_assigned: project,
+      time_allocated: time_allocated,
+      time_left: time_allocated
+    }
+    dept_data.push(dataObject)
+  }
+  console.log(dept_data)
 });
 
 
---
+++ static/js/dep_validation.js
@@ -0,0 +1,124 @@
+console.log("TEST")
+$(function(){
+    $.validator.setDefaults({
+        errorClass: "invalid-feedback",
+        highlight: function(element) {
+            $(element)
+            .closest('.form-control')
+            .addClass('is-invalid')
+            .removeClass('is-valid');
+        },
+        unhighlight: function(element) {
+            $(element)
+            .closest('.form-control')
+            .removeClass('is-invalid')
+            .addClass('is-valid');
+        }
+    }),
+    
+    $.validator.addMethod('totalHours', function(value, element) {
+        return parseInt(value)==8;
+    },'The total number of working should be equal to 8');
+
+    $("#dep_form").validate({  
+        rules: {
+            Process: {
+                required: true,
+                digits: true
+            },
+            Structure: {
+                required: true,
+                digits: true
+            },
+            Piping: {
+                required: true,
+                digits: true
+            },
+            Instrumentation: {
+                required: true,
+                digits: true
+            },
+            Electrical: {
+                required: true,
+                digits: true
+            },
+            Projects: {
+                required: true,
+                digits: true
+            },
+            Mechanical: {
+                required: true,
+                digits: true
+            },
+            Quality: {
+                required: true,
+                digits: true
+            },
+            Documentation: {
+                required: true,
+                digits: true
+            },
+            Planning: {
+                required: true,
+                digits: true
+            },
+        },
+        messages: {
+            Process: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+            Structure: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+            Piping: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+            Instrumentation: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+            Electrical: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+            Projects: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+            Mechanical: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+            Quality: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+            Documentation: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+            Planning: {
+                required: "This field is required",
+                digits: "You can only enter digits"
+            },
+        },
+        invalidHandler: function(event, validator) {
+            // 'this' refers to the form
+            var errors = validator.numberOfInvalids();
+            if (errors) {
+              alert(
+                "Please Correct the errors and try again."
+              )
+            } else {
+              $("div.error").hide();
+            }
+          },
+        //   submitHandler: function(form) {
+        //     $(form).ajaxSubmit();
+        //  }
+    });
+});
+
