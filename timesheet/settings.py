707eea0518392f9e4600cbaad8bc09d1ab8808e9
Ishant <ishantdahiya@gmail.com>
2020-06-28 15:36:01 +0530

    dashboard

--- timesheet/settings.py
+++ timesheet/settings.py
@@ -108,11 +108,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+MEDIA_URL = '/images/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = [
+STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static')
 ]
